ca5b88249ec3dc9b6cf853811d029674dcff5c47
TsvikAlexander <ipz203_tsos@student.ztu.edu.ua>
2021-02-02 17:59:15 +0200

    TodoList

---
+++ TodoList/Infrastructure/ToDoContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Models;
+
+namespace TodoList.Infrastructure
+{
+    public class ToDoContext : DbContext
+    {
+        public DbSet<TodoListItem> TodoListItems { get; set; }
+
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoListItem>().HasData(
+                new TodoListItem[]
+                {
+                new TodoListItem { Id=1, Title="Attend courses", AddDate=DateTime.Now, IsDone=false},
+                new TodoListItem { Id=2, Title="Go to the store", AddDate=DateTime.Now, IsDone=false},
+                new TodoListItem { Id=3, Title="Clean the house", AddDate=DateTime.Now, IsDone=true},
+                new TodoListItem { Id=4, Title="Learn programming", AddDate=DateTime.Now, IsDone=true}
+                });
+        }
+    }
+}
---
+++ TodoList/Migrations/20210131172242_InitDB.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoList.Migrations
+{
+    public partial class InitDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoListItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AddDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    IsDone = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoListItems", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TodoListItems",
+                columns: new[] { "Id", "AddDate", "IsDone", "Title" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2021, 1, 31, 19, 22, 41, 334, DateTimeKind.Local).AddTicks(2954), false, "Attend courses" },
+                    { 2, new DateTime(2021, 1, 31, 19, 22, 41, 339, DateTimeKind.Local).AddTicks(3938), false, "Go to the store" },
+                    { 3, new DateTime(2021, 1, 31, 19, 22, 41, 339, DateTimeKind.Local).AddTicks(4033), true, "Clean the house" },
+                    { 4, new DateTime(2021, 1, 31, 19, 22, 41, 339, DateTimeKind.Local).AddTicks(4042), true, "Learn programming" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoListItems");
+        }
+    }
+}
